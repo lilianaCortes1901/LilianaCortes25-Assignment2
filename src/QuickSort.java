10c1ad3b2af0c8a0e09009c3d0540a2598ceb0a4
lilianaCortes1901 <liliana.cortes@pop.belmont.edu>
2025-04-23 21:57:43 -0500

    Update to QuickSort.java
    Finished implementing QuickSort algorithm

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -4,7 +4,39 @@ public class QuickSort implements SortingInterface{
 
     @Override
     public int[] sorty(int[] input) {
-
+        if(input == null || input.length == 0){
+            return input;
+        }
+        quickSort(input, 0, input.length-1);
         return input;
+    }
+
+    private void quickSort(int[] list, int low, int high){
+        if (low < high) {
+            int piovt = partition(list, low, high); // partition index
+            quickSort(list, low, piovt - 1);  // Recursively sort left part
+            quickSort(list, piovt + 1, high); // Recursively sort right part
+        }
+    }
+
+    private int partition(int[] list, int low, int high) {
+        int pivot = list[high];
+        int i = low -1;
+
+        for(int j = low; j < high; j++){
+            if (list[j] <= pivot) {
+                i++;
+
+                // swap arr[i] and arr[j]
+                int temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+        }
+        int temp = list[i + 1];
+        list[i + 1] = list[high];
+        list[high] = temp;
+
+        return i + 1;
     }
 }
